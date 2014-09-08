51d40f71718c814c35131fe64e15e599983401b5
wangxiaomo <wxm4ever@gmail.com>
2014-09-08 14:53:40 +0800

    将时间戳存储到history中.

--- app/scripts/services/trackutilsservice.js
+++ app/scripts/services/trackutilsservice.js
@@ -8,7 +8,7 @@
  * Service in the smallblackdogApp.
  */
 angular.module('smallblackdogApp')
-  .service('trackUtilsService', function redditMusicService($rootScope, $localStorage) {
+  .service('trackUtilsService', function trackUtilsService($rootScope, $localStorage) {
 
   var getTrackMark = function(track) {
     return sprintf("%s:%s", track.domain, track.id);
@@ -16,18 +16,18 @@ angular.module('smallblackdogApp')
 
   this.init = function() {
     // FIXME: 短期内不重复播放.这个东西初始化和清理的策略是?
-    $localStorage.trackHistoryList = $localStorage.trackHistoryList || [];
+    $localStorage.trackHistoryList = $localStorage.trackHistoryList || {};
   };
 
   this.filterPlayedTrack = function(tracks) {
     return _.filter(tracks, function(track) {
-      return !(_.find($localStorage.trackHistoryList, getTrackMark(track)));
+      return !$localStorage.trackHistoryList[getTrackMark(track)];
     });
   };
 
   this.saveTrackHistory = function(track) {
     var mark = getTrackMark(track);
-    console.log(sprintf('save track %s', mark));
-    $localStorage.trackHistoryList.push(mark);
+    console.log(sprintf('save track %s history', mark));
+    $localStorage.trackHistoryList[mark] = Date.parse(new Date())/1000;
   };
 });
